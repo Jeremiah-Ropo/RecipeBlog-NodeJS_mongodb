fc6f55b4c3391a7b2c2e674927f3bebb1363adad
Patrick Jeremiah <patrickjerry59@yahoo.com>
2022-03-15 03:39:26 +0100

    in progress

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const express = require('express');
-const expressLayouts = require('express-ejs-layouts')
+const expressLayouts = require('express-ejs-layouts');
 
 
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-require('dotenv').config();
+require('dotenv').config();//For secret keys not to be seen on github
 
-//Middleware for templates
-app.use(express.urlencoded({ extended:true}));
+//MIDDLEWARE for templates
+app.use(express.urlencoded({ extended:true}));//Bodyparser
+
 //Hold on styles or images 
 app.use(express.static('public'));
+
+//EJS layout
 app.use(expressLayouts);
-
-
-//Layouts
 app.set('layout', './layouts/main' );
 app.set('view engine', 'ejs')
 
 //Routes
 const routes = require('./server/routes/recipeRoutes.js');
-app.set('/', routes);
+app.use('/', routes);
 
 app.listen(port, ()=> {
     console.log(`Listening to port ${port}`)
--- server/controllers/recipeController.js
+++ server/controllers/recipeController.js
@@ -1,6 +1,6 @@
 
 //Get homepage
-
+//Controller is more of where all router can be controlled.
 exports.homepage = async(req, res) => {
-    res.render('index');
+    res.render('home', { title: 'Homepage'});
 }
